d0b7961fb47652ab5d109be4a819174920e2bbc6
stonegitwin <531731134@qq.com>
2018-06-28 22:35:58 +0800

    First WPF

---
+++ WPFInDoor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFInDoor.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        CanvasViewModel _helloWorld;
+        public MainWindowViewModel()
+        {
+            _helloWorld = new CanvasViewModel();
+        }
+
+
+        public CanvasViewModel helloWord
+        {
+            get
+            {
+                return _helloWorld;
+            } 
+            set
+            {
+                _helloWorld = helloWord;
+            }
+        }
+    }
+}
